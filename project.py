dd563b59edff558bf1c3cf630472ea5892b8e001
Swapnil Ghosh <swapnil.ghosh@eduonix.com>
2019-07-25 11:29:09 +0530

    final count with individual data

--- project.py
+++ project.py
@@ -2,15 +2,16 @@ import xlrd
 
 path = 'statscounter_data.xlsx'
 count = 0
-check = 100;
+check = 600
+checkOne = 660
 wb = xlrd.open_workbook(path)
 print(len(wb.sheet_names()))
-for l in range(len(wb.sheet_names())):
-	sh = wb.sheet_by_index(l)
+for sh in wb.sheets():
+	# sh = wb.sheet_by_index(l)
 		# sh = wb.sheet_names()
 		# print(sh)
 		# sheet = wb.sheet_by_index(1)
-
+	perPageCnt = 0
 	for i in range(sh.nrows):
 		str = sh.cell_value(i,2)
 		res = [int(j) for j in str.split() if j.isdigit()]
@@ -23,6 +24,10 @@ for l in range(len(wb.sheet_names())):
 				else:
 					time = time + res[x]
 
-			if(time == check):
+			if(time >= check and time < checkOne):
 				count = count + 1
+				perPageCnt = perPageCnt + 1
+	print(sh.name)
+	print(perPageCnt)
+print("Total")
 print(count)
